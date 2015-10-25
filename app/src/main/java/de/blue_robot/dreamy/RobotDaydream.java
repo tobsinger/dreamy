c969cf790a966d9303a2acc62a58e316207198ed
Tobs <Tobs@Tobiass-MBP.fritz.box>
2015-10-25 09:21:42 +0100

    * Removed robot tutorial implementation

--- app/src/main/java/de/blue_robot/dreamy/RobotDaydream.java
+++ app/src/main/java/de/blue_robot/dreamy/RobotDaydream.java
@@ -1,11 +1,6 @@
 package de.blue_robot.dreamy;
 
-import java.util.Date;
-import java.util.Random;
-
-import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
-import android.graphics.Color;
 import android.graphics.Point;
 import android.service.dreams.DreamService;
 import android.view.View;
@@ -14,19 +9,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 
 import com.github.adnansm.timelytextview.TimelyView;
 import com.nineoldandroids.animation.ObjectAnimator;
 
+import java.util.Random;
+
 /**
  * Created by Tobs on 24/10/15.
  */
 public class RobotDaydream extends DreamService implements OnClickListener {
 
-    private Button dismissBtn;
     private TimelyView[] robotImgs;
     private AnimatorSet[] robotSets;
     private final int ROWS_COLS = 5;
@@ -79,40 +74,6 @@ public class RobotDaydream extends DreamService implements OnClickListener {
 
         Point screenSize = new Point();
         getWindowManager().getDefaultDisplay().getSize(screenSize);
-        int robotWidth = screenSize.x / ROWS_COLS;
-        int robotHeight = screenSize.y / ROWS_COLS;
-
-//        for (int r = 0; r < NUM_ROBOTS; r++) {
-//            //add to grid
-//            GridLayout.LayoutParams ddP = new GridLayout.LayoutParams();
-//            ddP.width = robotWidth;
-//            ddP.height = robotHeight;
-//
-//            if (r == randPosn) {
-//                //stop button
-//                dismissBtn = new Button(this);
-//                dismissBtn.setText("stop");
-//                dismissBtn.setBackgroundColor(Color.WHITE);
-//                dismissBtn.setTextColor(Color.RED);
-//                dismissBtn.setOnClickListener(this);
-//                dismissBtn.setLayoutParams(ddP);
-//                ddLayout.addView(dismissBtn);
-//            } else {
-//                //robot image view
-//                robotImgs[r] = new TimelyView(this);
-//                objectAnimator = robotImgs[r].animate(-1, 15);
-//                objectAnimator.setDuration(DURATION);
-//// robotImgs[r].animate(1, 15);
-////                robotImgs[r].setImageResource(R.drawable.ic_launcher);
-//                ddLayout.addView(robotImgs[r], ddP);
-//
-//                robotSets[r] = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.android_spin);
-//
-//                robotSets[r].setTarget(robotImgs[r]);
-//                robotImgs[r].setOnClickListener(this);
-//
-//            }
-//        }
 
         setContentView(R.layout.timely_layout);
 
