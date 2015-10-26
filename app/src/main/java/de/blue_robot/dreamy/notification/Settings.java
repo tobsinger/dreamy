77591a67c2c4f00ec5ee7a0b6bf4f0534ac126cc
Tobs <Tobs@Tobiass-MacBook-Pro.local>
2015-10-26 21:28:24 +0100

    * Fixed  accessibility service enabled check

--- app/src/main/java/de/blue_robot/dreamy/notification/Settings.java
+++ app/src/main/java/de/blue_robot/dreamy/notification/Settings.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import de.blue_robot.dreamy.RobotDaydream;
+import de.blue_robot.dreamy.listener.NotificationListener;
 
 /**
  * Created by Tobs on 26/10/15.
@@ -19,9 +20,9 @@ public class Settings {
     private RobotDaydream context;
 
     public void activateService() {
-        final Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (!this.isAccessibilityEnabled()) {
+            final Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         }
     }
@@ -29,16 +30,15 @@ public class Settings {
     /**
      * determines if the accessibility service is enabled
      *
-     * @return
+     * @return true if accessibility service is enabled
      */
     public boolean isAccessibilityEnabled() {
         int accessibilityEnabled = 0;
-        final String LIGHTFLOW_ACCESSIBILITY_SERVICE = "de.bluerobot.flashnotification/de.bluerobot.flashnotification.Notificationdetector";
+        final String notificationListener = NotificationListener.class.getCanonicalName();
         final boolean accessibilityFound = false;
         try {
             accessibilityEnabled = android.provider.Settings.Secure.getInt(this.context.getContentResolver(),
                     android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
-            Log.d(TAG, "ACCESSIBILITY: " + accessibilityEnabled);
         } catch (final android.provider.Settings.SettingNotFoundException e) {
             Log.d(TAG,
                     "Error finding setting, default accessibility to not found: " + e.getMessage());
@@ -48,28 +48,20 @@ public class Settings {
                 ':');
 
         if (accessibilityEnabled == 1) {
-            Log.d(TAG, "***ACCESSIBILIY IS ENABLED***: ");
 
             final String settingValue = android.provider.Settings.Secure.getString(this.context.getContentResolver(),
                     android.provider.Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-            Log.d(TAG, "Setting: " + settingValue);
             if (settingValue != null) {
                 final TextUtils.SimpleStringSplitter splitter = mStringColonSplitter;
                 splitter.setString(settingValue);
                 while (splitter.hasNext()) {
                     final String accessabilityService = splitter.next();
-                    Log.d(TAG, "Setting: " + accessabilityService);
-                    if (accessabilityService.equalsIgnoreCase(LIGHTFLOW_ACCESSIBILITY_SERVICE)) {
-                        Log.d(TAG,
-                                "We've found the correct setting - accessibility is switched on!");
+                    if (accessabilityService.contains(notificationListener)) {
                         return true;
                     }
                 }
             }
 
-            Log.d(TAG, "***END***");
-        } else {
-            Log.d(TAG, "***ACCESSIBILIY IS DISABLED***");
         }
         return accessibilityFound;
     }
