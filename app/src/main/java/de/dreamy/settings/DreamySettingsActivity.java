acb5b643582a3832fb0bc922fb2027c6cc7480e5
Tobias Berger <tobe@wstutobe01.local>
2016-08-23 12:03:01 +0200

    fixing severe layout error

--- app/src/main/java/de/dreamy/settings/DreamySettingsActivity.java
+++ app/src/main/java/de/dreamy/settings/DreamySettingsActivity.java
@@ -10,12 +10,14 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 import javax.inject.Inject;
